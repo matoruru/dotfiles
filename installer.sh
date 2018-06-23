43912502a2947f42301299bf8447239e8ad270ae
matoruru <matoruru@shgt.jp>
2018-06-23 12:49:16 +0900

    add a rc files installer

--- installer.sh
+++ installer.sh
@@ -10,19 +10,40 @@ if [[ "$EUID" = 0 ]]; then
    exit
 fi
 
+function create_oldfile {
+   mv $1 $1.old
+}
+
 # install rc files
-cp .bash_profile ~/
-cp .bashrc       ~/
-mkdir -p      ~/.config/fish
-cp    .fishrc ~/.config/fish/config.fish
-ln    -s      ~/.config/fish/config.fish ~/.fishrc
+if [ -f $HOME/.bash_profile ]; then
+   create_oldfile $HOME/.bash_profile
+fi
+ln -sr .bash_profile ~/
+
+if [ -f $HOME/.bashrc ]; then
+   create_oldfile $HOME/.bashrc
+fi
+ln -sr .bashrc ~/
+
+FISHDIR="$HOME/.config/fish"
+if [ -d $FISHDIR ]; then
+   if [ -f $FISHDIR/config.fish ]; then
+      create_oldfile $FISHDIR/config.fish
+   fi
+else
+   mkdir -p $FISHDIR
+fi
+ln -sr .fishrc $FISHDIR/config.fish
+ln -sr         $FISHDIR/config.fish ~/.fishrc
+
+# install vimrc
+cp .vimrc ~/
 
 # install xmonad
 cp    /etc/X11/xinit/xinitrc   ~/.xinitrc
 cat  .xinitrc                > ~/.xinitrc
 mkdir           ~/.xmonad
 cp    xmonad.hs ~/.xmonad/
-
 
 # install urxvtc (rxvt-unicode as a daemon)
 cp .Xresources ~/
