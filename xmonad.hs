e0903f9c4752e25ff04722d7e2b10968e99b53d6
matoruru <matoruru@shgt.jp>
2018-07-24 08:36:23 +0000

    add spacing around eath windows

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,17 @@
 import XMonad
+import XMonad.Config.Desktop
+import XMonad.Layout.Spacing
 
-main = xmonad $ defaultConfig {
-      terminal    = myTerminal
-     ,modMask     = myModMask
-     ,borderWidth = myBorderWidth
-    }
+main = xmonad desktopConfig
+   {
+    terminal    = myTerminal
+   ,modMask     = myModMask
+   ,borderWidth = myBorderWidth
+   ,layoutHook  = mySpacing
+   }
 
 myTerminal    = "urxvtc" -- use as a daemon
 myModMask     = mod4Mask -- Win key or Super_L
 myBorderWidth = 1
+mySpacing     = spacing 2 $ Tall 1 (3/100) (1/2) ||| Full
+
