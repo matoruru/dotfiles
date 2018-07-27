f1fff8c97cc56679068b739c6a420536bfbbcdd3
matoruru <matoruru@shgt.jp>
2018-07-27 19:59:41 +0900

    add xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig
 
 myTerminal    = "urxvtc" -- use as a daemon
 myModMask     = mod4Mask -- Win key or Super_L
@@ -11,12 +13,13 @@ gwU      = 24 -- bacause height of dmenu is 18.
 gwD      = 6
 gwL      = 6
 gwR      = 6
-mySpacing   = spacing gapwidth $ gaps [(U, gwU), (D, gwD), (L, gwL), (R, gwR)] $ Tall 1 0.05 0.5 ||| Full
-main = xmonad defaultConfig
+mySpacing   = spacing gapwidth $ gaps [(U, gwU), (D, gwD), (L, gwL), (R, gwR)] $ Tall 1 0.05 0.5 ||| Tall 2 0.05 0.5 ||| Full
+myKeysP = [ ("M-p", spawn "rofi -show run") ]
+
+main = xmonad =<< xmobar ( defaultConfig
    {
     terminal    = myTerminal
    ,modMask     = myModMask
    ,borderWidth = myBorderWidth
    ,layoutHook  = mySpacing
-   }
-
+   } `additionalKeysP` myKeysP )
