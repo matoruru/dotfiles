ccf17ce545f9dedb49e19e8452f68ff0107412c8
matoruru <40382980+matoruru@users.noreply.github.com>
2019-12-04 12:07:44 +0900

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -81,42 +81,112 @@ bLight = spawn . ("xbacklight " ++) . (++ " -time 1")
 amixer = spawn . ("amixer set Master " ++)
 
 myKeysP :: [(String, X ())]
-myKeysP = [ ("M-u"      , spawn myTerminal)
-          , ("M-s"      , myBrowser)
-          , ("M-p"      , appLauncher)
-          , ("<Print>"  , screenShot)
-          , ("S-<Print>", screenShot')
-          , ("M-S-m", windows W.swapMaster)
-          , ("M-h"  , moveTo Prev NonEmptyWS)
-          , ("M-l"  , moveTo Next NonEmptyWS)
-          , ("M-S-l", moveTo Next EmptyWS   )
-          , ("M-S-h", moveTo Prev EmptyWS   )
-          , ("M-<Tab>"  , nextScreen)
-          , ("M-S-<Tab>", prevScreen)
-          , ("M-<L>", sendMessage Shrink      )
-          , ("M-<R>", sendMessage Expand      )
-          , ("M-<U>", sendMessage MirrorExpand)
-          , ("M-<D>", sendMessage MirrorShrink)
-          , ("M-0"  , do setScreenSpacing border
-                         setWindowSpacing border)
-          , ("M-S-=", decScreenWindowSpacing 4)
-          , ("M--"  , incScreenWindowSpacing 4)
-          , ("M-<XF86ApplicationRight>" , bLight "+5"  )
-          , ("<XF86MonBrightnessUp>"    , bLight "+5"  )
-          , ("<XF86MonBrightnessDown>"  , bLight "-5"  )
-          , ("S-<XF86MonBrightnessUp>"  , bLight "+100")
-          , ("S-<XF86MonBrightnessDown>", bLight "-100")
-          , ("<XF86AudioRaiseVolume>", amixer "1%+"   )
-          , ("<XF86AudioLowerVolume>", amixer "1%-"   )
-          , ("<XF86AudioMute>"       , amixer "toggle")
-          , ("M-S-c"       , return ())
-          , ("M-<Return>"  , return ())
-          , ("M-S-<Return>", return ())
-          , ("M-S-r", reCompile)
-          , ("M-r"  , reStart  )
-          , ("M-S-q", return() )
-          , ("M-q"  , return() )
-          , ("M-c", kill)
+myKeysP = [ ( "M-u"
+            , spawn myTerminal
+            )
+          , ( "M-s"
+            , myBrowser
+            )
+          , ( "M-p"
+            , appLauncher
+            )
+          , ( "<Print>"
+            , screenShot
+            )
+          , ( "S-<Print>"
+            , screenShot'
+            )
+          , ( "M-S-m"
+            , windows W.swapMaster
+            )
+          , ( "M-h"
+            , moveTo Prev NonEmptyWS
+            )
+          , ( "M-l"
+            , moveTo Next NonEmptyWS
+            )
+          , ( "M-S-l"
+            , moveTo Next EmptyWS
+            )
+          , ( "M-S-h"
+            , moveTo Prev EmptyWS
+            )
+          , ( "M-<Tab>"
+            , nextScreen
+            )
+          , ( "M-S-<Tab>"
+            , prevScreen
+            )
+          , ( "M-<L>"
+            , sendMessage Shrink
+            )
+          , ( "M-<R>"
+            , sendMessage Expand
+            )
+          , ( "M-<U>"
+            , sendMessage MirrorExpand
+            )
+          , ( "M-<D>"
+            , sendMessage MirrorShrink
+            )
+          , ("M-0"
+            , do setScreenSpacing border
+                 setWindowSpacing border
+            )
+          , ( "M-S-="
+            , decScreenWindowSpacing 4
+            )
+          , ( "M--"
+            , incScreenWindowSpacing 4
+            )
+          , ( "M-<XF86ApplicationRight>"
+            , bLight "+5"
+            )
+          , ( "<XF86MonBrightnessUp>"
+            , bLight "+5"
+            )
+          , ( "<XF86MonBrightnessDown>"
+            , bLight "-5"
+            )
+          , ( "S-<XF86MonBrightnessUp>"
+            , bLight "+100"
+            )
+          , ( "S-<XF86MonBrightnessDown>"
+            , bLight "-100"
+            )
+          , ( "<XF86AudioRaiseVolume>"
+            , amixer "1%+"
+            )
+          , ( "<XF86AudioLowerVolume>"
+            , amixer "1%-"
+            )
+          , ( "<XF86AudioMute>"
+            , amixer "toggle"
+            )
+          , ( "M-S-c"
+            , return ()
+            )
+          , ( "M-<Return>"
+            , return ()
+            )
+          , ( "M-S-<Return>"
+            , return ()
+            )
+          , ( "M-S-r"
+            , reCompile
+            )
+          , ( "M-r"
+            , reStart
+            )
+          , ( "M-S-q"
+            , return()
+            )
+          , ( "M-q"
+            , return()
+            )
+          , ( "M-c"
+            , kill
+            )
           ]
 
 myKeysToRemove :: [(ButtonMask, Button)]
@@ -143,7 +213,7 @@ myManageHook = composeAll
          ]
 
 data WsState
-  = Here
+  = Current
   | NotEmpty
   | Empty
 
@@ -158,13 +228,13 @@ getWsLog = do
          idx          = flip (-) 1 . read
          sortById ids = unzip . sortOn fst . zip ids
          fmt cw ws wi
-            | wi == cw              = Here
+            | wi == cw              = Current
             | isJust $ ws !! idx wi = NotEmpty
             | otherwise             = Empty
 
 stateToSym :: WsState -> String
 stateToSym = \case
-  Here     -> "\63022"
+  Current  -> "\63022"
   NotEmpty -> "\61842"
   Empty    -> "\63023"
 
