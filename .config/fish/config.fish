a006676c43a64ecee91644f3c38fdec74b58f369
matoruru <matoruru@shgt.jp>
2021-03-21 04:56:51 +0900

    Add docker command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,26 @@ bash ~/repositories/matoruru/arch-tools/checktools.sh
 # Set alias
 alias nvimtutor="nvim -c Tutor"
 
+function dls
+    set -l green (tput setaf 2)
+    set -l default (tput sgr0)
+    echo $green"[Images]"$default
+    docker image ls
+    echo
+    echo $green"[Running containers]"$default
+    docker container ls
+end
+
+function dla
+    set -l green (tput setaf 2)
+    set -l default (tput sgr0)
+    echo $green"[All images (including intermediates)]"$default
+    docker image ls --all
+    echo
+    echo $green"[All containers]"$default
+    docker container ls --all
+end
+
 # Set tab step
 tabs 3
 
